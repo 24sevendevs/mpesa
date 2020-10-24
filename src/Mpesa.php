617c2f12eb56fabbd40a5e3adabd4fe5fc0cf666
kelvinthiongo <thiongokelvin5@gmail.com>
2020-10-24 14:58:33 +0300

    B2C 1.2.0

--- src/Mpesa.php
+++ src/Mpesa.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace TFS\Mpesa;
+
 use Zttp\Zttp;
+
+use function GuzzleHttp\json_decode;
 
 // use function GuzzleHttp\json_decode;
 
@@ -9,9 +12,9 @@ class Mpesa
 {
     public static function get_access_token()
     {
-        $token_url = \Config::get("mpesa.".config('mpesa.mode').".token_url");
-        $consumer_key = \Config::get("mpesa.".config('mpesa.mode').".consumer_key");
-        $consumer_secret = \Config::get("mpesa.".config('mpesa.mode').".consumer_secret");
+        $token_url = \Config::get("mpesa." . config('mpesa.mode') . ".token_url");
+        $consumer_key = \Config::get("mpesa." . config('mpesa.mode') . ".consumer_key");
+        $consumer_secret = \Config::get("mpesa." . config('mpesa.mode') . ".consumer_secret");
 
 
         $response = Zttp::withBasicAuth($consumer_key, $consumer_secret)->get($token_url);
@@ -19,27 +22,26 @@ class Mpesa
         $access_token = json_decode($response, true)['access_token'];
 
         return $access_token;
-
     }
 
     public static function mpesa_express($phone, $amount, $AccountReference, $TransactionDesc, $callback = null)
     {
-        if($phone == "" || $phone == null || $amount == "" || $amount == null || $AccountReference == "" || $AccountReference == null || $TransactionDesc == "" || $TransactionDesc == null){
+        if ($phone == "" || $phone == null || $amount == "" || $amount == null || $AccountReference == "" || $AccountReference == null || $TransactionDesc == "" || $TransactionDesc == null) {
             return response()->json([
                 "error" => "invalid data. All parameters must not be null"
             ], 403);
         }
-        if(!$callback){
+        if (!$callback) {
             $callback = config('mpesa.callback_url');
         }
         $access_token = Mpesa::get_access_token();
 
         $time = date('YmdHis');
-        $shortcode = \Config::get("mpesa.".config('mpesa.mode').".shortcode");
-        $passkey = \Config::get("mpesa.".config('mpesa.mode').".passkey");
+        $shortcode = \Config::get("mpesa." . config('mpesa.mode') . ".shortcode");
+        $passkey = \Config::get("mpesa." . config('mpesa.mode') . ".passkey");
         $password = base64_encode($shortcode . $passkey . $time);
 
-        $stkpush_url = \Config::get("mpesa.".config('mpesa.mode').".stkpush_url");
+        $stkpush_url = \Config::get("mpesa." . config('mpesa.mode') . ".stkpush_url");
 
         $headers = [
             'Authorization' => 'Bearer ' . $access_token,
@@ -67,15 +69,68 @@ class Mpesa
         return json_decode($response, true);
     }
 
+    public static function c2b($phone, $amount, $occassion, $remarks, $callback = null, $command_id = null)
+    {
+        if ($phone == "" || $phone == null || $amount == "" || $amount == null || $occassion == "" || $occassion == null || $remarks == "" || $remarks == null) {
+            return response()->json([
+                "error" => "invalid data. All parameters must not be null"
+            ], 403);
+        }
+        if (!$callback) {
+            $callback = config('mpesa.callback_url');
+        }
+        if (!$command_id) {
+            $command_id = "BusinessPayment";
+        } elseif ($command_id != "SalaryPayment" && $command_id != "BusinessPayment" && $command_id != "PromotionPayment") {
+            return response()->json([
+                "error" => "invalid data. Command ID can only be SalaryPayment, BusinessPayment or PromotionPayment"
+            ], 403);
+        }
+        $access_token = Mpesa::get_access_token();
+
+        $initiator_name = \Config::get("mpesa." . config('mpesa.mode') . ".initiator_name");
+        $initiator_password = \Config::get("mpesa." . config('mpesa.mode') . ".initiator_password");
+        $shortcode = \Config::get("mpesa." . config('mpesa.mode') . ".shortcode");
+
+        $publicKey = file_get_contents(__DIR__ . "/assets/public_keycert.cer");
+        openssl_public_encrypt($initiator_password, $encrypted, $publicKey, OPENSSL_PKCS1_PADDING);
+        //GENERATE SECURITY CREDENTIAL USING THE $publicKey
+        $security_credential = base64_encode($encrypted);
+
+        $b2c_url = \Config::get("mpesa." . config('mpesa.mode') . ".b2c_url");
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $access_token,
+            'Content-Type' => 'application/json',
+            // 'Host: sandbox.safaricom.co.ke'
+        ];
+        $data = [
+            "InitiatorName" => $initiator_name,
+            "SecurityCredential" => $security_credential,
+            "CommandID" => $command_id,
+            "Amount" => $amount,
+            "PartyA" => $shortcode,
+            "PartyB" => $phone,
+            "ResultURL" => $callback,
+            "QueueTimeOutURL" => $callback . "/timeout",
+            "Remarks" => $remarks,
+            "Occassion" => $occassion
+        ];
+        $response = Zttp::withHeaders($headers)->post($b2c_url, $data);
+
+
+        return json_decode($response, true);
+    }
+
     public static function query_request($CheckoutRequestID)
     {
         $access_token = Mpesa::get_access_token();
         $time = date('YmdHis');
-        $shortcode = \Config::get("mpesa.".config('mpesa.mode').".shortcode");
-        $passkey = \Config::get("mpesa.".config('mpesa.mode').".passkey");
+        $shortcode = \Config::get("mpesa." . config('mpesa.mode') . ".shortcode");
+        $passkey = \Config::get("mpesa." . config('mpesa.mode') . ".passkey");
         $password = base64_encode($shortcode . $passkey . $time);
 
-        $query_url = \Config::get("mpesa.".config('mpesa.mode').".stkquery_url");
+        $query_url = \Config::get("mpesa." . config('mpesa.mode') . ".stkquery_url");
         $headers = [
             'Authorization' => 'Bearer ' . $access_token,
             'Content-Type' => 'application/json',
